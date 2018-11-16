89734ddcafab6da5d365638dc9c8c016edc99b84
saeedsami <saeedsami333@gmail.com>
2018-11-17 01:56:06 +0600

    my profile design fixed

--- resources/views/users/myProfile.blade.php
+++ resources/views/users/myProfile.blade.php
@@ -7,6 +7,14 @@
    
 
   <section class="section-profile">
+    <div class=" profile_nav">
+        <div class=" nav_items">
+        <a href="" class="items">books</a>
+        <a href="" class="items">books</a>
+        <a href="" class="items">books</a>
+        <a href="" class="items">books</a>
+        </div>
+    </div>
    <div class="row"> 
        <div class="col-1-of-3">
                    
@@ -32,46 +40,35 @@
 
         </div>
 <!-- right side -->
-       <div class="col-2-of-3" >
-           <div class="profile-details">
-               <a href="index.html" class="btn-text .u-margin-btn  "> <i class=" icon fas fa-book"> &nbsp;</i>my book</a>
+<div class="col-2-of-3" >
+    <div class="books">
+<div class="mybook">
+        <img class="book-image" src="{{ asset('images/paul-schafer.jpg') }}" alt="">
+        <div class="description">
+                <a href="#" class=" .u-margin-btn right "> <i class=" icon fas fa-heart"> &nbsp;favourite</i></a>
+                <h3 class="heading-tertiary u-margin-bottom-small ">Book Title </h3>
+                <p class="paragraph">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae libero a atque laborum tempore, eum ipsa tempora aspernatur omnis, repellendus quos harum blanditiis praesentium. Repudiandae tempore doloremque fugit maiores doloribus!            
+                </p> 
+                    <div class="buttons">
+        <a href="#" class="btn-items .u-margin-btn  "> <i class=" icon fab fa-angellist"> &nbsp;</i>request book</a>
+        <a href="#" class="btn-items .u-margin-btn  "> <i class=" icon fab fa-angellist"> &nbsp;</i>suggest book</a>
+        <a href="#" class="btn-items .u-margin-btn  "> <i class=" icon fas fa-edit"> &nbsp;</i>edit book</a>
+        <a href="#" class=" .u-margin-btn right"> <i class=" icon fas fa-trash-alt"> &nbsp;</i></a>
 
-               <a href="#" class="btn-text .u-margin-btn"><i class=" icon fas fa-book"> &nbsp;</i>favourite book</a>
+</div>
+        
 
-               <a href="#" class="btn-text .u-margin-btn"><i class=" icon fas fa-book"> &nbsp;</i>suggest book</a>
-                
-               <a href="#" class="btn-text .u-margin-btn">upload book</a>
-
-           </div>  
-           
             
-            <div class="books">
-                   <div class="mybook">
-                           <img class="book-image" src="img/book2.jpg" alt="">
-                           <div class="description">
-                                   <a href="#" class=" .u-margin-btn right "> <i class=" icon fas fa-heart"> &nbsp;favourite</i></a>
-                                  <h3 class="heading-tertiary u-margin-bottom-small ">Book Title </h3>
-                                    <p class="paragraph">
-                                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae libero a atque laborum tempore, eum ipsa tempora aspernatur omnis, repellendus quos harum blanditiis praesentium. Repudiandae tempore doloremque fugit maiores doloribus!            
-                                    </p> 
-                                     <div class="buttons">
-                           <a href="#" class="btn-text .u-margin-btn  "> <i class=" icon fab fa-angellist"> &nbsp;</i>request book</a>
-                           <a href="#" class="btn-text .u-margin-btn  "> <i class=" icon fab fa-angellist"> &nbsp;</i>suggest book</a>
-
-
-                           <a href="#" class=" .u-margin-btn right "> <i class=" icon fas fa-trash-alt"> &nbsp;</i></a>
-                           <a href="#" class="btn-text .u-margin-btn  "> <i class=" icon fas fa-edit"> &nbsp;</i>edit book</a>
-
-                    </div>
-                           </div>
-                    </div>
-
-                   
-            </div>
-        </div>
+    </div>
+</div>
    </div>
    </div>
+
+    
+     
   </section>
- 
+  
+
   
 @endsection
