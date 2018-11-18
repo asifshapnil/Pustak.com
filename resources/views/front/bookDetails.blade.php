9632fed034a515bc9f3ac9755de161783acdfec5
asif <asif.shapnil@gmail.com>
2018-11-18 16:09:02 +0600

    my rating update

--- resources/views/front/bookDetails.blade.php
+++ resources/views/front/bookDetails.blade.php
@@ -7,192 +7,201 @@
     @include('front.layouts.book_details_tab')
 @endif
 
+
 <section class="section-about">
+<div class="row">
+<div class="col-1-of-2 ">
+    <div class="details">
+        {{-- <a href="#" class=" u-margin-btn right "> <i class="textred icon fas fa-heart"> &nbsp;favourite</i></a> --}}
 
-    <section class="section-about">
-            <div class="row">
-                 <div class="col-1-of-2 ">
-                     <div class="details">
-                            <h3 class="heading-tertiary u-margin-bottom-small u-padding-top">Book Name &rarr;</h3>
-                            <p class="paragraph">
-                                The True story 
-                            </p>
+        <h3 class="heading-tertiary u-margin-bottom-small u-padding-top">Book Name &rarr;      
+            <a href="#" class=" u-margin-btn right "> <i class="textred icon fas fa-heart"> &nbsp;favourite</i></a>
+        </h3>
+        <p class="paragraph">
+            The True story 
+        </p>
+
+        <h3 class="heading-tertiary u-margin-bottom-small">Author Name &rarr;   Mahmud Al Saeed</h3>
+        
+        <h3 class="heading-tertiary u-margin-bottom-small">Edition &rarr;  The True story  2018</h3>
+        
+
+        <h3 class="heading-tertiary u-margin-bottom-small">Condition &rarr;  Good </h3>
+        
+        <h3 class="heading-tertiary u-margin-bottom-medium ">ISBN No. &rarr; 123456789 </h3>
     
-                            <h3 class="heading-tertiary u-margin-bottom-small">Author Name &rarr;</h3>
-                            <p class="paragraph">
-                                Mahmud Al Saeed
-                            </p> 
-                            <h3 class="heading-tertiary u-margin-bottom-small">Edition &rarr;</h3>
-                            <p class="paragraph">
-                                The True story  2018
-                            </p>
-    
-                            <h3 class="heading-tertiary u-margin-bottom-small">Condition &rarr;</h3>
-                            <p class="paragraph">
-                               Good 
-                            </p>
+        <a href="#popup" class="btn-text">request Now &rarr;</a>
+        <a href="#popup" class="btn-text .u-margin-btn  "> <i class=" icon fab fa-angellist"> &nbsp;</i>suggest book</a>
 
-                            <h3 class="heading-tertiary u-margin-bottom-small">ISBN No. &rarr;</h3>
-                            <p class="paragraph">
-                               123456789 
-                            </p>
-    
-                            <a href="#popup" class="btn-text">request Now &rarr;</a>
-                            <a href="#popup" class="btn-text .u-margin-btn  "> <i class=" icon fab fa-angellist"> &nbsp;</i>suggest book</a>
+        <div class="star u-margin-bottom-small">
+                 {{-- rating start --}}
+            <fieldset class="rating ">
+                    <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
+                    <input type="radio" id="star4half" name="rating" value="4 and a half" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
+                    <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
+                    <input type="radio" id="star3half" name="rating" value="3 and a half" /><label class="half" for="star3half" title="Meh - 3.5 stars"></label>
+                    <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
+                    <input type="radio" id="star2half" name="rating" value="2 and a half" /><label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
+                    <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
+                    <input type="radio" id="star1half" name="rating" value="1 and a half" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label>
+                    <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
+                    <input type="radio" id="starhalf" name="rating" value="half" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
+                </fieldset>
+    {{-- rating end --}}
+        </div>
 
-                     </div>
+        </div>                
+        </div>
+        <div class="col-1-of-2">
+            <div class="composition">
 
-                         
-                 </div>
-                 <div class="col-1-of-2">
-                     <div class="composition">
+            @foreach ($details->image as $key=>$image)
+                @php
+                    if($key == 0){
+                        $image1 = '';
+                        $image1 = $image->image;
+                    }elseif($key == 1){
+                        $image2 = '';
+                        $image2 = $image->image;
+                    }elseif($key == 2){
+                        $image3 = '';
+                        $image3 = $image->image;
+                    }else{
 
-                        @foreach ($details->image as $key=>$image)
-                            @php
-                                if($key == 0){
-                                    $image1 = '';
-                                    $image1 = $image->image;
-                                }elseif($key == 1){
-                                    $image2 = '';
-                                    $image2 = $image->image;
-                                }elseif($key == 2){
-                                    $image3 = '';
-                                    $image3 = $image->image;
-                                }else{
+                    }
+                @endphp
+            @endforeach
 
-                                }
-                            @endphp
-                        @endforeach
-
-                        @if(isset($image1))
-                        
-                         <img 
-                         srcset="images/{{ $image1 }} 300w, images/{{ $image1 }} 1000w,"
-                         sizes="(max-width:65.25em) 20vw,(max-width:37.5em) 30vw,300px"
-                         class="composition__photo composition__photo--p1"
-                         src="images/{{ $image1 }}" 
-                         alt="photo 1"
-                         >
-                         @endif
-                         @if(isset($image2))
-                        
-                         <img 
-                         srcset="images/{{ $image2 }} 300w, images/{{ $image2 }} 1000w,"
-                         sizes="(max-width:65.25em) 20vw,(max-width:37.5em) 30vw,300px"
-                         class="composition__photo composition__photo--p2"
-                         src="images/{{ $image2 }}" 
-                         alt="photo 2"
-                         >
-                         @endif
-                        @if(isset($image3))
-                         <img 
-                         srcset="images/{{ $image3 }} 300w, images/{{ $image3 }} 1000w,"
-                         sizes="(max-width:65.25em) 20vw,(max-width:37.5em) 30vw,300px"
-                         class="composition__photo composition__photo--p3"
-                         src="images/{{ $image3 }}" 
-                         alt="photo 3"
-                         > 
-                         @endif    
-                     </div>
-                 </div>
-             </div>
+            @if(isset($image1))
+            
+                <img 
+                srcset="images/{{ $image1 }} 300w, images/{{ $image1 }} 1000w,"
+                sizes="(max-width:65.25em) 20vw,(max-width:37.5em) 30vw,300px"
+                class="composition__photo composition__photo--p1"
+                src="images/{{ $image1 }}" 
+                alt="photo 1"
+                >
+                @endif
+                @if(isset($image2))
+            
+                <img 
+                srcset="images/{{ $image2 }} 300w, images/{{ $image2 }} 1000w,"
+                sizes="(max-width:65.25em) 20vw,(max-width:37.5em) 30vw,300px"
+                class="composition__photo composition__photo--p2"
+                src="images/{{ $image2 }}" 
+                alt="photo 2"
+                >
+                @endif
+            @if(isset($image3))
+                <img 
+                srcset="images/{{ $image3 }} 300w, images/{{ $image3 }} 1000w,"
+                sizes="(max-width:65.25em) 20vw,(max-width:37.5em) 30vw,300px"
+                class="composition__photo composition__photo--p3"
+                src="images/{{ $image3 }}" 
+                alt="photo 3"
+                > 
+                @endif    
+            </div>
+        </div>
+    </div>
 
 
          
-         <div class="book-about">
-                     
-                
-                <div class="row">
-                        <div class="col-2-of-3">
-                                <h3 class="heading-tertiary u-margin-bottom-small storyname">Author Name </h3>
-
-                                <div class="text-area u-margin-bottom-small">
-                                    <textarea rows="8" cols="50" class="text-details">
-                                        I HAD THE BEST WEEK EVER WITH MY FAMILY
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis laborum explicabo impedit optio tempore voluptas earum eum, soluta eveniet dolorum ullam officiis nobis unde repellendus. Culpa suscipit officiis voluptatum qui.                                         </textarea>
-                                </div>
-                               <div class="save-button u-margin-bottom-medium">
-                                <a class="save " href="">save</a>
-                               </div> 
-
-                        </div>
-                        
-                        <div class="col-1-of-3">
-                         <div class="public">
-                            <div class="row">
-                                <div class="story">
-                                    <figure class="story__shape">
-                                         <img src="img/nat-8.jpg" alt="person on a tour" class="story__img">
-                                          <figcaption class="story__caption">Jack Wilson </figcaption>
-                                        </figure>
-                                    <div class="story__text">
-                                        <h3 class="heading-tertiary u-margin-bottom-small"> completely different now</h3>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                        </p>
-                                    </div>
+<div class="book-about">
             
-                                </div>
-                            </div>
-                         </div>
-                        </div>
+    
+    <div class="row">
+            <div class="col-2-of-3">
+                    <h3 class="heading-tertiary u-margin-bottom-small storyname">Author Name </h3>
+
+                    <div class="text-area u-margin-bottom-small">
+                        <textarea rows="8" cols="50" class="text-details">
+                            I HAD THE BEST WEEK EVER WITH MY FAMILY
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis laborum explicabo impedit optio tempore voluptas earum eum, soluta eveniet dolorum ullam officiis nobis unde repellendus. Culpa suscipit officiis voluptatum qui.                                         </textarea>
                     </div>
-                    <!-- why you need me -->
+                    <div class="save-button u-margin-bottom-medium">
+                    <a class="save " href="">save</a>
+                    </div> 
+
+            </div>
+            
+            <div class="col-1-of-3">
+                <div class="public">
+                <div class="row">
+                    <div class="story">
+                        <figure class="story__shape">
+                                <img src="img/nat-8.jpg" alt="person on a tour" class="story__img">
+                                <figcaption class="story__caption">Jack Wilson </figcaption>
+                            </figure>
+                        <div class="story__text">
+                            <h3 class="heading-tertiary u-margin-bottom-small"> completely different now</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        <!-- why you need me -->
+        <div class="row">
+            <div class="col-2-of-3">
+                <h3 class="heading-tertiary u-margin-bottom-small why-should-i">Why Should  You Take From Me </h3>
+
+                <div class="text-area u-margin-bottom-small">
+                    <textarea rows="8" cols="50" class="text-details">
+                        I HAD THE BEST WEEK EVER WITH MY FAMILY
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis laborum explicabo impedit optio tempore voluptas earum eum, soluta eveniet dolorum ullam officiis nobis unde repellendus. Culpa suscipit officiis voluptatum qui.     
+                        </textarea>
+                </div>
+                <div class="save-button u-margin-bottom-medium">
+                    <a class="save " href="">save</a>
+                </div>
+            </div>
+            
+            <div class="col-1-of-3">
+                <div class="public">
                     <div class="row">
-                        <div class="col-2-of-3">
-                            <h3 class="heading-tertiary u-margin-bottom-small why-should-i">Why Should  You Take From Me </h3>
+                        <div class="story">
+                            <figure class="story__shape">
+                                    <img src="img/nat-9.jpg" alt="person on a tour" class="story__img">
+                                    <figcaption class="story__caption">Jack Wilson </figcaption>
+                                </figure>
+                            <div class="story__text">
+                                <h3 class="heading-tertiary u-margin-bottom-small"> completely different now</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
 
-                            <div class="text-area u-margin-bottom-small">
-                                <textarea rows="8" cols="50" class="text-details">
-                                    I HAD THE BEST WEEK EVER WITH MY FAMILY
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis laborum explicabo impedit optio tempore voluptas earum eum, soluta eveniet dolorum ullam officiis nobis unde repellendus. Culpa suscipit officiis voluptatum qui.                                     </textarea>
                             </div>
-                            <div class="save-button u-margin-bottom-medium">
-                                <a class="save " href="">save</a>
-                            </div>
-                        </div>
-                        
-                        <div class="col-1-of-3">
-                            <div class="public">
-                                <div class="row">
-                                    <div class="story">
-                                        <figure class="story__shape">
-                                             <img src="img/nat-9.jpg" alt="person on a tour" class="story__img">
-                                              <figcaption class="story__caption">Jack Wilson </figcaption>
-                                            </figure>
-                                        <div class="story__text">
-                                            <h3 class="heading-tertiary u-margin-bottom-small"> completely different now</h3>
-                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-
-                                        </div>
-                
-                                    </div>
-                                </div>
-                             </div>
+    
                         </div>
                     </div>
+                    </div>
+            </div>
+        </div>
 
-                   
+        
 
                     
                 
                 
 
-         </div> 
-         <div class="comment">
-                  <h3 class="heading-tertiary u-margin-bottom-small u-padding-top storyname">Your Comment &rarr;</h3>
+    </div> 
+    <div class="comment">
+            <h3 class="heading-tertiary u-margin-bottom-small u-padding-top storyname">Your Comment &rarr;</h3>
 
-             <textarea class="comment__details  u-margin-bottom-small" name="comment" id="" cols="65" rows="7">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-             </textarea>
-             <div class="save-button u-margin-bottom-medium">
-                    <a class="save " href="">save</a>
-                </div>
-         </div>
-                                    
-         
-        </section>
+        <textarea class="comment__details  u-margin-bottom-small" name="comment" id="" cols="65" rows="7">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </textarea>
+        <div class="save-button u-margin-bottom-medium">
+            <a class="save " href="">save</a>
+        </div>
+    </div>
+                            
+    
+</section>
 
         
         
--- resources/views/users/myProfile.blade.php
+++ resources/views/users/myProfile.blade.php
@@ -45,7 +45,7 @@
 <div class="mybook">
         <img class="book-image" src="{{ asset('images/paul-schafer.jpg') }}" alt="">
         <div class="description">
-                <a href="#" class=" .u-margin-btn right "> <i class=" icon fas fa-heart"> &nbsp;favourite</i></a>
+                <a href="#" class="u-margin-btn right "> <i class=" textred icon fas fa-heart"> &nbsp;favourite</i></a>
                 <h3 class="heading-tertiary u-margin-bottom-small ">Book Title </h3>
                 <p class="paragraph">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae libero a atque laborum tempore, eum ipsa tempora aspernatur omnis, repellendus quos harum blanditiis praesentium. Repudiandae tempore doloremque fugit maiores doloribus!            
@@ -55,6 +55,22 @@
         <a href="#" class="btn-items .u-margin-btn  "> <i class=" icon fab fa-angellist"> &nbsp;</i>suggest book</a>
         <a href="#" class="btn-items .u-margin-btn  "> <i class=" icon fas fa-edit"> &nbsp;</i>edit book</a>
         <a href="#" class=" .u-margin-btn right"> <i class=" icon fas fa-trash-alt"> &nbsp;</i></a>
+        <div class="star u-margin-bottom-small">
+                {{-- rating start --}}
+           <fieldset class="rating ">
+                   <input type="radio" id="star5" name="rating" value="5" /><label class = "full" for="star5" title="Awesome - 5 stars"></label>
+                   <input type="radio" id="star4half" name="rating" value="4 and a half" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
+                   <input type="radio" id="star4" name="rating" value="4" /><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
+                   <input type="radio" id="star3half" name="rating" value="3 and a half" /><label class="half" for="star3half" title="Meh - 3.5 stars"></label>
+                   <input type="radio" id="star3" name="rating" value="3" /><label class = "full" for="star3" title="Meh - 3 stars"></label>
+                   <input type="radio" id="star2half" name="rating" value="2 and a half" /><label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
+                   <input type="radio" id="star2" name="rating" value="2" /><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
+                   <input type="radio" id="star1half" name="rating" value="1 and a half" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label>
+                   <input type="radio" id="star1" name="rating" value="1" /><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
+                   <input type="radio" id="starhalf" name="rating" value="half" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
+               </fieldset>
+   {{-- rating end --}}
+       </div>
 
 </div>
         
