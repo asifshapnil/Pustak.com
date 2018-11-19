444e46d57552c12e992decbff5d86183ba9c1351
saeedsami <saeedsami333@gmail.com>
2018-11-19 23:53:41 +0600

    logo design

--- resources/views/users/layouts/nav.blade.php
+++ resources/views/users/layouts/nav.blade.php
@@ -1,5 +1,9 @@
 
     <div class="primary-nav ">
+
+     <div class="logo">
+     <a href="{{ route('front-home') }}"><img class="logoimg" src="{{ asset('images/logo.png') }}" alt="logo"></a>
+     </div>
         <div class=" primary-nav__search-input">
                 <input class="search-input__text" type="text" placeholder="Type to search">
                 <a class="search-input__btn" href=""><i class="icon fas fa-search"></i></a>
